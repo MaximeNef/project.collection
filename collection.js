b028d1be0a9ab616a67832ea73b331dec98101e4
MaximeNef <maximenef1@gmail.com>
2020-11-12 12:03:37 +0100

    derniere modification avant js

---
+++ collection.js
@@ -0,0 +1,75 @@
+const collection = [
+    {
+        title: 'bonjour',
+        author: 'vald',
+        category: ['hip-hop','rap'],
+        link: 'https://www.youtube.com/watch?v=x-VYdBKwSLc',
+        description: "Vald, de son vrai nom Valentin Le Du, né le 15 juillet 1992 à Aulnay-sous-Bois, en Seine-Saint-Denis, est un rappeur et parolier français."
+
+    },
+    {
+        title: "docteur c'est chouette ",
+        author: 'laurent garnier',
+        category: ['house music'],
+        link: 'https://www.youtube.com/watch?v=Q8wzavO4vHs',
+        description: 'Laurent Garnier, né le 1ᵉʳ février 1966 à Boulogne-Billancourt, est un DJ, compositeur et producteur de musique électronique français.'
+    },
+    {
+        title: 'one more time',
+        author: 'daft punk',
+        category: ['dance','électronique'],
+        link: 'https://www.youtube.com/watch?v=FGBhQbmPwH8',
+        description: 'Daft Punk, est un groupe de musique électronique français, originaire de Paris.'
+    },
+    {
+        title: 'nirvana',
+        author: 'doc gyneco',
+        category: ['hip-hop','rap'],
+        link: 'https://www.youtube.com/watch?v=zFKzgtkUhPU',
+        description: 'Doc Gynéco, de son vrai nom Bruno Beausir, né le 10 mai 1974 à Clichy-sous-Bois, en Seine-Saint-Denis, est un auteur-compositeur-interprète, chanteur, rappeur et producteur français.'
+    },
+    {
+        title: 'ma dope',
+        author: 'neekfeu',
+        category: ['hip-hop','rap'],
+        link: 'https://www.youtube.com/watch?v=Z68u6dJqoI0',
+        description: 'Nekfeu, de son vrai nom Ken Samaras, né le 3 avril 1990 à La Trinité, dans les Alpes-Maritimes, est un rappeur et acteur français.'
+    },
+    {
+        title: 'woop woop',
+        author: 'paxel',
+        category: ['dance','électronique'],
+        link: 'https://www.youtube.com/watch?v=cS2c3mMqVNA',
+        description: "j'ai rien trouvé sur lui sorry."
+    },
+    {
+        title: 'iceland',
+        author: 'ppetit biscuit',
+        category: ['hoouse music'],
+        link: 'https://www.youtube.com/watch?v=VepeQJDBEek',
+        description: 'Mehdi Benjelloun dit Petit Biscuit, né le 10 novembre 1999 à Rouen, est un DJ et producteur de musique électronique français.'
+    },
+    {
+        title: 'heartless',
+        author: 'milky chance',
+        category: ['rock alternatif'],
+        link: 'https://www.youtube.com/watch?v=78pRdbSgreM',
+        description: " Milky Chance est un groupe d'Indie pop allemand, originaire de Cassel. Leur style musical est influencé par le reggae et la musique électronique."
+    },
+    {
+        title: "c'ets pas grave",
+        author: 'columbine',
+        category: ['hip-hop','rap'],
+        link: 'https://www.youtube.com/watch?v=_yyjPxvNLGk',
+        description: 'Columbine est un collectif de rap français originaire de Rennes, en Bretagne, formé en 2014.'
+    },
+    {
+        title: 'champion',
+        author: 'kayne west',
+        category: ['hip-hop','rap'],
+        link: 'https://www.youtube.com/watch?v=L1SEEMkc-qw',
+        description: 'Kanye West, de son nom complet Kanye Omari West /ˈkɑːnjeɪ wɛst/, né le 8 juin 1977 à Atlanta, en Géorgie, est un rappeur, auteur-compositeur-interprète, réalisateur artistique.'
+    },
+
+
+]
